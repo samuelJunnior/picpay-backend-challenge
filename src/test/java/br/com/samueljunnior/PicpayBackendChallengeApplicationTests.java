506d5212757a95fd9e66e429b3ec03869c39abbc
samuel <cast.samuel.uitc@sebrae.com.br>
2024-07-02 16:47:50 -0300

    Initial commit

---
+++ src/test/java/br/com/samueljunnior/PicpayBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samueljunnior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
